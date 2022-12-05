ae8101920b3050016566e341aa0ccf6e1f578d42
StefanCapG1IOTD1 <s142268@ap.be>
2022-12-05 16:31:54 +0100

    cappi

---
+++ js/mapScript.js
@@ -0,0 +1,21 @@
+/* In dit script plaats je de code om de kaart in de id apMap te tekenen, zodat de AP-Hogeschool met adres Ellermanstraat 33 gecentreerd staat.  
+* Gebruik hiervoor de documentatie op https://leafletjs.com/ 
+*/
+
+let apMap = L.map('apMap').setView([51.23009, 4.41616], 17);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoibGVjdG9yd291dGVyIiwiYSI6ImNrM25qZWs1dTB4NHgza240bW0zOG1qZngifQ.1uF5JjJA8l5SpTW3NVQJJQ'
+}).addTo(apMap);
+
+
+// bepaal de rechthoek rondom het gebouw van AP
+let bounds = [[51.23041, 4.4155], [51.22991, 4.41675]];
+// kleur de rechthoek in met de rode AP-kleur
+L.rectangle(bounds, {color: "#e60005", weight: 1}).addTo(apMap);
+// plaats een marker met als tekst "AP-Hogeschool" en eronder "Ellermanstraat 33"
+let apMarker = L.marker([51.23009, 4.41616]).addTo(apMap);
+apMarker.bindPopup("<b>IOT bedrijf 4CAPS</b><br>Ellermanstraat 33").openPopup();
